4c45f32fdadfd9e497294e335bed61595cdb8bd9
Francisco Rojas <josefcorojas@gmail.com>
2012-03-28 14:59:39 +0800

    section 5.3 done

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
   def full_title(page_title)
     base_title = "Ruby on Rails Tutorial Sample App"
     if page_title.empty?
-    base_title
+      base_title
     else
       "#{base_title} | #{page_title}"
     end
