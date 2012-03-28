949b0933214dc15d42e20f0dc98b78ee6a88d996
Francisco Rojas <josefcorojas@gmail.com>
2012-03-28 15:27:10 +0800

    Finished ch5

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 RailsTutorial::Application.routes.draw do
+  get "users/new"
+
   root to: 'static_pages#home' # match '/', to: 'static_pages#home' -- get "static_pages/home"
   match '/help',    to: 'static_pages#help' # get "static_pages/help"
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
+  match '/signup', to: 'users#new'
+  get "users/new"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
