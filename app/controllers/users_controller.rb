df5d81d656861e79d19dea33c67ccc9fde9a8ddb
Francisco Rojas <josefcorojas@gmail.com>
2012-04-16 20:07:52 +0800

    Section 7.3 done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,16 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-  
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      # Handle a successful save.
+      else
+      render 'new'
+    end
+  end
+
   def show
     @user = User.find(params[:id])
   end
