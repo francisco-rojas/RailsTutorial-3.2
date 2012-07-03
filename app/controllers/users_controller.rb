4177dd4e4582e8488ace0d16111e9f1b912f9047
Francisco Rojas <josefcorojas@gmail.com>
2012-07-03 12:15:07 +0800

    section 10.2 done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,6 +28,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
   
   def index
