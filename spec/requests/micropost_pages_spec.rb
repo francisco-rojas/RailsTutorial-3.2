8d9d72a9c213ffbb6b4a59861f3f43e70659b495
Francisco Rojas <josefcorojas@gmail.com>
2012-07-04 09:43:13 +0800

    chapter 9 done

--- spec/requests/micropost_pages_spec.rb
+++ spec/requests/micropost_pages_spec.rb
@@ -29,4 +29,16 @@ subject { page }
       end
     end
   end
+  
+  describe "micropost destruction" do
+    before { FactoryGirl.create(:micropost, user: user) }
+
+    describe "as correct user" do
+      before { visit root_path }
+
+      it "should delete a micropost" do
+        expect { click_link "delete" }.should change(Micropost, :count).by(-1)
+      end
+    end
+  end
 end
